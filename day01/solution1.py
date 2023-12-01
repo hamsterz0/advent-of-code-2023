8a4d0d28877904e2d413b829b356a0e87846118a
arnavgarg <thearnavgarg@gmail.com>
2023-12-01 01:10:47 -0800

    day1

--- day01/solution1.py
+++ day01/solution1.py
@@ -10,21 +10,37 @@ def sol1():
 
 
 def sol2():
-    # '''
-    # one
-    # two
-    # three
-    # four
-    # five
-    # six
-    # seven
-    # eight
-    # nine
-    # '''
-    word_mapping = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    word_mapping = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
     word_list = word_mapping.keys()
+    numbers = list('123456789')
+
+    current_sum = 0
     
-    first_idx, last_idx = None, None
-    first_number, last_number = None, None
+    for line in lines:
+        first_idx, second_idx = 1000, -1
+        first_number, second_number = None, None
+        for number in numbers:
+            loc1 = line.find(number)
+            loc2 = line.rfind(number)
+            if loc1 != -1 and (loc1 < first_idx):
+                    first_idx = loc1
+                    first_number = number
+            if loc2 != -1 and (loc2 > second_idx):
+                second_idx = loc2
+                second_number = number
+        
+        for word in word_list:
+            loc1 = line.find(word)
+            loc2 = line.rfind(word)
+            if loc1 != -1 and (loc1 < first_idx):
+                first_idx = loc1
+                first_number = word_mapping[word]
+            if loc2 != -1 and (loc2 > second_idx):
+                second_idx = loc2
+                second_number = word_mapping[word]
+
+        # print(first_number, second_number)
+        current_sum += int(first_number + second_number)
+    print(current_sum)
 
 sol2()
